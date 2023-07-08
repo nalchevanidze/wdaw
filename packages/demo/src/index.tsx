830cfa93f4cc3db58b29fe7ba319c0e4c0cdd1ac
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-07-08 14:37:48 +0200

    waw

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -8,10 +8,26 @@ const type = urlParams.get("type");
 
 createRoot(document.getElementById("app")!).render(
   <div>
-    {type === "player" ? (
-      <Player src="https://nalchevanidze.com/assets/audio/david-alpha-black-hole" />
-    ) : (
-      <Synth />
-    )}
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "20px",
+      }}
+    >
+      <a href="/?type=player">player</a>
+      <a href="/?type=synth">synth</a>
+    </nav>
+    <div>
+      {type === "player" ? (
+        <Player
+          width={500}
+          height={500}
+          src="https://nalchevanidze.com/assets/audio/david-alpha-black-hole"
+        />
+      ) : (
+        <Synth />
+      )}
+    </div>
   </div>
 );
