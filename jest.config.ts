af4f048c83a8178f326dee1127d5c94561a4e0d4
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-07-09 15:52:14 +0200

    jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const ignore = ['/node_modules/', '/dist/', '/generated/'];
+
+const config: Config = {
+  preset: 'ts-jest',
+  coverageDirectory: 'test-report/coverage',
+  collectCoverage: true,
+  collectCoverageFrom: [`<rootDir>/packages/**/src/**/*.ts`],
+  coveragePathIgnorePatterns: ignore,
+  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
+  rootDir: './',
+  testPathIgnorePatterns: ignore
+};
+
+module.exports = config;
