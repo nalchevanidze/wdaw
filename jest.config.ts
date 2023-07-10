0e4d1289b2b19051164f16fc675c0bc37220eca1
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-07-10 23:10:41 +0200

    no reporter

--- jest.config.ts
+++ jest.config.ts
@@ -6,17 +6,7 @@ const config: Config = {
   preset: 'ts-jest',
   // coverageDirectory: 'test-report/coverage',
   // collectCoverage: true,
-  // reporters: [
-  //   'default',
-  //   ['github-actions', { silent: false }],
-  //   [
-  //     './node_modules/jest-html-reporter',
-  //     {
-  //       pageTitle: 'Test Report',
-  //       outputPath: 'test-report/index.html'
-  //     }
-  //   ]
-  // ],
+  reporters: ['default', ['github-actions', { silent: false }]],
   // collectCoverageFrom: [`<rootDir>/packages/**/src/**/*.ts`],
   // coveragePathIgnorePatterns: ignore,
   // coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
