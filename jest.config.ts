742a50975b3432bfe6f20a52cc073e74cf9d58dc
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-07-10 23:07:23 +0200

    no-coverage

--- jest.config.ts
+++ jest.config.ts
@@ -4,22 +4,22 @@ const ignore = ['/node_modules/', '/dist/', '/generated/'];
 
 const config: Config = {
   preset: 'ts-jest',
-  coverageDirectory: 'test-report/coverage',
-  collectCoverage: true,
-  reporters: [
-    'default',
-    ['github-actions', { silent: false }],
-    [
-      './node_modules/jest-html-reporter',
-      {
-        pageTitle: 'Test Report',
-        outputPath: 'test-report/index.html'
-      }
-    ]
-  ],
-  collectCoverageFrom: [`<rootDir>/packages/**/src/**/*.ts`],
-  coveragePathIgnorePatterns: ignore,
-  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
+  // coverageDirectory: 'test-report/coverage',
+  // collectCoverage: true,
+  // reporters: [
+  //   'default',
+  //   ['github-actions', { silent: false }],
+  //   [
+  //     './node_modules/jest-html-reporter',
+  //     {
+  //       pageTitle: 'Test Report',
+  //       outputPath: 'test-report/index.html'
+  //     }
+  //   ]
+  // ],
+  // collectCoverageFrom: [`<rootDir>/packages/**/src/**/*.ts`],
+  // coveragePathIgnorePatterns: ignore,
+  // coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
   rootDir: './',
   testPathIgnorePatterns: ignore
 };
