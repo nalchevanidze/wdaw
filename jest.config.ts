a49be710e3b055ee17a75abe92694c7ebd9c9bc7
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-07-10 22:55:01 +0200

    reporters

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   preset: 'ts-jest',
   coverageDirectory: 'test-report/coverage',
   collectCoverage: true,
+  reporters: ['default', ['github-actions', { silent: false }]],
   collectCoverageFrom: [`<rootDir>/packages/**/src/**/*.ts`],
   coveragePathIgnorePatterns: ignore,
   coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
