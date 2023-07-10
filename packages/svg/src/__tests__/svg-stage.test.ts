297170565dd87852539a5472daa8a11d06bb8437
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-07-10 22:42:33 +0200

    fix format

--- packages/svg/src/__tests__/svg-stage.test.ts
+++ packages/svg/src/__tests__/svg-stage.test.ts
@@ -17,35 +17,35 @@ describe('#radianToDegree', () => {
 describe('#posToDegree', () => {
   const point1 = { x: 1, y: 1 };
 
-  test('degree of: point(1,1) point(2,2) is 45 dgree', () => {
+  test('degree of: point(1,1) point(2,2) is 45 degree', () => {
     expect(posToDegree(point1, { x: 2, y: 2 })).toBeCloseTo(45);
   });
 
-  test('degree of: point(1,1) point(1,2) is 90 dgree', () => {
+  test('degree of: point(1,1) point(1,2) is 90 degree', () => {
     expect(posToDegree(point1, { x: 1, y: 2 })).toBeCloseTo(90);
   });
 
-  test('degree of: point(1,1) point(0,2) is 135 dgree', () => {
+  test('degree of: point(1,1) point(0,2) is 135 degree', () => {
     expect(posToDegree(point1, { x: 0, y: 2 })).toBeCloseTo(135);
   });
 
-  test('degree of: point(1,1) point(0,1) is 180 dgree', () => {
+  test('degree of: point(1,1) point(0,1) is 180 degree', () => {
     expect(posToDegree(point1, { x: 0, y: 1 })).toBeCloseTo(180);
   });
 
-  test('degree of: point(1,1) point(0,0) is 225 dgree', () => {
+  test('degree of: point(1,1) point(0,0) is 225 degree', () => {
     expect(posToDegree(point1, { x: 0, y: 0 })).toBeCloseTo(225);
   });
 
-  test('degree of: point(1,1) point1(1,0) is 270 dgree', () => {
+  test('degree of: point(1,1) point1(1,0) is 270 degree', () => {
     expect(posToDegree(point1, { x: 1, y: 0 })).toBeCloseTo(270);
   });
 
-  test('degree of: point(1,1) point(2,0) is 315 dgree', () => {
+  test('degree of: point(1,1) point(2,0) is 315 degree', () => {
     expect(posToDegree(point1, { x: 2, y: 0 })).toBeCloseTo(315);
   });
 
-  test('degree of: point(1,1) point(2,1) is 360 dgree', () => {
+  test('degree of: point(1,1) point(2,1) is 360 degree', () => {
     expect(posToDegree(point1, { x: 2, y: 1 })).toBeCloseTo(360);
   });
 });
