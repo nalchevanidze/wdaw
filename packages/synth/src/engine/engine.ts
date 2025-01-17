e812d769db53199417616259cb212db1b94ca1fa
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-17 15:12:01 +0100

    update

--- packages/synth/src/engine/engine.ts
+++ packages/synth/src/engine/engine.ts
@@ -5,12 +5,24 @@ import { Midi, PLAYER_ACTION } from '../core/types';
 import { Synth } from './synth';
 import { NoteAction } from './midi/types';
 
-class CoreEngine {
+export class SynthEngine implements SoundIterator {
   private synth = new Synth();
   private player = new MidiPlayer(this.synth);
 
-  setMidiCallback(f: MidiCallback) {
+  private actions: NoteAction[];
+  private preset: Preset;
+  private closeContext: () => void;
+
+  constructor() {
+    this.closeContext = audioProcessor(this);
+  }
+
+  public setMidiCallback(f: MidiCallback) {
     this.player.onChange = f;
+  }
+
+  public setPreset(preset: Preset) {
+    this.preset = preset;
   }
 
   public setPlay(mode: PLAYER_ACTION) {
@@ -26,62 +38,30 @@ class CoreEngine {
     }
   }
 
-  public startNote(preset: Preset, n: number) {
-    this.synth.startNote(preset, n);
+  public startNote(n: number) {
+    this.synth.startNote(this.preset, n);
     this.player.refresh();
   }
 
-  public endNote(preset: Preset, n: number) {
-    this.synth.endNote(preset, n);
+  public endNote(n: number) {
+    this.synth.endNote(this.preset, n);
     this.player.refresh();
   }
 
   public destroy() {
     this.player.setTime(0);
     this.synth.clear();
+    this.closeContext();
   }
 
   public setTime = (t: number) => this.player.setTime(t);
 
-  public next(preset: Preset, actions: NoteAction[]) {
-    const action = this.player.next(actions);
+  public next() {
     this.player.refresh();
-    return this.synth.next(preset, action);
+    return this.synth.next(this.preset, this.player.next(this.actions));
   }
-}
-
-export class SynthEngine implements SoundIterator {
-  private engine = new CoreEngine();
-  private actions: NoteAction[];
-  private closeContext: () => void;
-  private preset: Preset;
-
-  constructor() {
-    this.closeContext = audioProcessor(this);
-  }
-
-  public setPlay = (mode: PLAYER_ACTION) => this.engine.setPlay(mode);
-
-  public endNote = (n: number) => this.engine.endNote(this.preset, n);
-
-  public startNote = (n: number) => this.engine.startNote(this.preset, n);
 
   public setMidi = (midi: Midi): void => {
     this.actions = toActions(midi);
   };
-
-  public setMidiCallback = (f: MidiCallback) => this.engine.setMidiCallback(f);
-
-  public setPreset(preset: Preset) {
-    this.preset = preset;
-  }
-
-  public setTime = (t: number) => this.engine.setTime(t);
-
-  public next = () => this.engine.next(this.preset, this.actions);
-
-  public destroy() {
-    this.engine.destroy();
-    this.closeContext();
-  }
 }
