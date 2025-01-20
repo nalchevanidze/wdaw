76f8489d7039df405983ac583857cc80fe5413e6
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-20 07:42:07 +0100

    initial midi

--- packages/synth/src/engine/state/index.ts
+++ packages/synth/src/engine/state/index.ts
@@ -2,5 +2,3 @@ export { presetNames } from './presets';
 export type { PresetName } from './presets';
 export type { DAWState } from './state';
 export { getPreset, dawState } from './state';
-
-export { prelude as initialMidi } from './midi';
