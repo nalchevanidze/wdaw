d464cf456dbda2807c8391149eeb45db89589371
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-20 07:11:48 +0100

    update

--- packages/synth/src/engine/state/midi.ts
+++ packages/synth/src/engine/state/midi.ts
@@ -1,62 +1,6 @@
 import { Midi, Note } from '../types';
 
-const prelude: Midi = {
-  size: 16,
-  notes: {
-    '0': [
-      { at: 0, id: 'C#3', length: 32 },
-      { at: 0, id: 'F#2', length: 32 },
-      { at: 0, id: 'D#1', length: 32 }
-    ],
-    '4': [
-      { at: 0, id: 'C#3', length: 32 },
-      { at: 0, id: 'F#2', length: 32 },
-      { at: 0, id: 'D#1', length: 32 }
-    ],
-    '8': [
-      { at: 0, id: 'F#2', length: 32 },
-      { at: 0, id: 'D#2', length: 32 },
-      { at: 0, id: 'C#1', length: 32 }
-    ],
-    '12': [
-      { at: 0, id: 'F#2', length: 32 },
-      { at: 0, id: 'D#2', length: 32 },
-      { at: 0, id: 'B0', length: 32 }
-    ]
-  }
-};
-
-const bass: Midi = {
-  size: 16,
-  notes: {
-    '0': [
-      { at: 4, id: 'D#2', length: 4 },
-      { at: 12, id: 'D#2', length: 4 },
-      { at: 20, id: 'D#2', length: 4 },
-      { at: 28, id: 'D#2', length: 4 }
-    ],
-    '4': [
-      { at: 4, id: 'D#2', length: 4 },
-      { at: 12, id: 'D#2', length: 4 },
-      { at: 20, id: 'D#2', length: 4 },
-      { at: 28, id: 'D#2', length: 4 }
-    ],
-    '8': [
-      { at: 4, id: 'C#2', length: 4 },
-      { at: 12, id: 'C#2', length: 4 },
-      { at: 20, id: 'C#2', length: 4 },
-      { at: 28, id: 'C#2', length: 4 }
-    ],
-    '12': [
-      { at: 4, id: 'F#2', length: 4 },
-      { at: 12, id: 'F#2', length: 4 },
-      { at: 20, id: 'F#2', length: 4 },
-      { at: 28, id: 'F#2', length: 4 }
-    ]
-  }
-};
-
-const midiLoop = (f: () => Note[]) => ({
+const midiLoop = (f: () => Note[]):Midi => ({
   size: 16,
   notes: {
     '0': f(),
@@ -66,4 +10,4 @@ const midiLoop = (f: () => Note[]) => ({
   }
 });
 
-export { prelude, bass, midiLoop };
+export {   midiLoop };
--- packages/synth/src/engine/state/state.ts
+++ packages/synth/src/engine/state/state.ts
@@ -1,7 +1,7 @@
 import { Preset } from '../common/types';
 import { Midi } from '../types';
 
-import { bass, prelude, midiLoop } from './midi';
+import { midiLoop } from './midi';
 import { PresetName, presets } from './presets';
 
 type PlayerState = {
@@ -36,13 +36,65 @@ export const dawState = (): DAWState => {
     {
       name: 'piano',
       preset: getPreset('pluck'),
-      midi: prelude,
+      midi: {
+        size: 16,
+        notes: {
+          '0': [
+            { at: 0, id: 'C#3', length: 32 },
+            { at: 0, id: 'F#2', length: 32 },
+            { at: 0, id: 'D#1', length: 32 }
+          ],
+          '4': [
+            { at: 0, id: 'C#3', length: 32 },
+            { at: 0, id: 'F#2', length: 32 },
+            { at: 0, id: 'D#1', length: 32 }
+          ],
+          '8': [
+            { at: 0, id: 'F#2', length: 32 },
+            { at: 0, id: 'D#2', length: 32 },
+            { at: 0, id: 'C#1', length: 32 }
+          ],
+          '12': [
+            { at: 0, id: 'F#2', length: 32 },
+            { at: 0, id: 'D#2', length: 32 },
+            { at: 0, id: 'B0', length: 32 }
+          ]
+        }
+      },
       gain: 0.4
     },
     {
       name: 'bass',
       preset: getPreset('bass'),
-      midi: bass,
+      midi: {
+        size: 16,
+        notes: {
+          '0': [
+            { at: 4, id: 'D#2', length: 4 },
+            { at: 12, id: 'D#2', length: 4 },
+            { at: 20, id: 'D#2', length: 4 },
+            { at: 28, id: 'D#2', length: 4 }
+          ],
+          '4': [
+            { at: 4, id: 'D#2', length: 4 },
+            { at: 12, id: 'D#2', length: 4 },
+            { at: 20, id: 'D#2', length: 4 },
+            { at: 28, id: 'D#2', length: 4 }
+          ],
+          '8': [
+            { at: 4, id: 'C#2', length: 4 },
+            { at: 12, id: 'C#2', length: 4 },
+            { at: 20, id: 'C#2', length: 4 },
+            { at: 28, id: 'C#2', length: 4 }
+          ],
+          '12': [
+            { at: 4, id: 'F#2', length: 4 },
+            { at: 12, id: 'F#2', length: 4 },
+            { at: 20, id: 'F#2', length: 4 },
+            { at: 28, id: 'F#2', length: 4 }
+          ]
+        }
+      },
       gain: 0.2
     },
     {
