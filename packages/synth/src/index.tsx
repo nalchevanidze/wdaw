0e12fc9dd4ef6212aaf0ebf950420e780579e81c
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-22 12:37:59 +0100

    UI

--- packages/synth/src/index.tsx
+++ packages/synth/src/index.tsx
@@ -1,39 +1,10 @@
 import * as React from 'react';
-import { Synth } from './ui/synth';
-import { PianoRoll } from './ui/piano-roll';
-import { Keyboard } from './ui/keyboard';
 import { Configurator } from './ui/configurator';
-import { colors } from './ui/styles';
-import { Tracks } from './ui/tracks';
-
-const styles = {
-  daw: {
-    display: 'flex',
-    position: 'relative',
-    justifyContent: 'center',
-    fontFamily: 'sans-serif'
-  },
-  synth: {
-    width: '660px',
-    height: '435px',
-    borderRadius: '2px',
-    background: colors.background,
-    border: '1px solid #CCC'
-  }
-} as const;
+import { UI } from './ui';
 
 const DAW: React.FC = () => (
   <Configurator>
-    <div style={styles.daw}>
-      <section style={styles.synth}>
-        <Synth />
-        <Keyboard />
-      </section>
-      <div style={{ position: 'relative' }}>
-        <PianoRoll />
-        <Tracks />
-      </div>
-    </div>
+    <UI />
   </Configurator>
 );
 
---
+++ packages/synth/src/ui/index.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Synth } from './synth';
+import { PianoRoll } from './piano-roll';
+import { Keyboard } from './keyboard';
+import { colors } from './styles';
+import { Tracks } from './tracks';
+
+const styles = {
+  daw: {
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    fontFamily: 'sans-serif'
+  },
+  synth: {
+    width: '660px',
+    height: '435px',
+    borderRadius: '2px',
+    background: colors.background,
+    border: '1px solid #CCC'
+  }
+} as const;
+
+const UI: React.FC = () => (
+  <>
+    <div style={styles.daw}>
+      <section style={styles.synth}>
+        <Synth />
+        <Keyboard />
+      </section>
+      <div style={{ position: 'relative' }}>
+        <PianoRoll />
+        <Tracks />
+      </div>
+    </div>
+  </>
+);
+
+export { UI };
