ddd44d584f0ef15bfd290753e54ee0644ac972e1
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-23 20:39:54 +0100

    update

---
+++ packages/synth/src/ui/hooks/useNotes.ts
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { flatten, deepen, Selected } from '../piano-roll/utils';
+import { useContext, useState } from 'react';
+import { NotePoint } from '../types';
+import { ConfiguratorContext } from '../configurator';
+
+export const useNotes = () => {
+  const [
+    {
+      tracks: { currentTrack, tracks }
+    },
+    dispatch
+  ] = useContext(ConfiguratorContext);
+
+  const [notes, setNotes] = useState<Selected<NotePoint>>({
+    selected: [],
+    inactive: flatten(tracks[currentTrack].midi)
+  });
+
+  const updateNotes = (ns: Selected<NotePoint>) => {
+    setNotes(ns);
+    if (ns.selected.length === 0) {
+      dispatch({
+        type: 'SET_MIDI',
+        payload: deepen(
+          [...ns.selected, ...ns.inactive],
+          tracks[currentTrack].midi
+        )
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    setNotes({
+      selected: [],
+      inactive: flatten(tracks[currentTrack].midi)
+    });
+  }, [tracks[currentTrack].midi]);
+
+  return { notes, updateNotes };
+};
