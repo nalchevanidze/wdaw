c635beff77731d8804c142df519422ba6c0dd8a1
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-26 00:05:06 +0100

    loopTime

--- packages/synth/src/ui/hooks/use-time.ts
+++ packages/synth/src/ui/hooks/use-time.ts
@@ -1,22 +1,23 @@
 import { useContext } from 'react';
 import { ConfiguratorContext } from '../configurator';
-import { NOTE_SIZE, NOTE_STEP } from '../common/defs';
-
+import { NOTE_SIZE, SUB_QUARTER } from '../common/defs';
 
 export const useTime = () => {
   const [{ player, tracks }] = useContext(ConfiguratorContext);
   const midi = tracks.tracks[tracks.currentTrack].midi;
-  const loopSize = (midi.loop[1] - midi.loop[0]) * NOTE_SIZE;
-  const start = midi.start * NOTE_SIZE;
-  const end = midi.end * NOTE_SIZE;
-  const t = player.time;
-  const loopStart = midi.loop[0] * NOTE_SIZE;
+  const loopSize = midi.loop[1] - midi.loop[0];
+  const start = midi.start;
+  const end = midi.end;
+  const time = player.time / NOTE_SIZE;
+  const loopStart = midi.loop[0];
   const offset = start % loopSize;
 
-  const time =
-    t < start || t > end
-      ? 0
-      : (loopStart - offset + (t % loopSize)) * NOTE_STEP;
+  const loopTime =
+    time < start || time > end ? 0 : loopStart - offset + (time % loopSize);
 
-  return { time, loop: midi.loop, end: midi.end * NOTE_SIZE };
+  return {
+    time: loopTime * SUB_QUARTER,
+    loop: midi.loop,
+    end: midi.end * NOTE_SIZE
+  };
 };
