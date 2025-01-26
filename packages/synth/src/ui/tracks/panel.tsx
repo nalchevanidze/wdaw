893f2ce6e44377f7f496045b10a9bd0fac9827f4
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-26 12:43:09 +0100

    update

--- packages/synth/src/ui/tracks/panel.tsx
+++ packages/synth/src/ui/tracks/panel.tsx
@@ -8,17 +8,24 @@ type Props = {
   y: number;
   active: boolean;
   width: number;
-  height: number
+  height: number;
 };
 
-export const Panel: React.FC<Props> = ({ name, id, active, y, width, height }) => {
+export const Panel: React.FC<Props> = ({
+  name,
+  id,
+  active,
+  y,
+  width,
+  height
+}) => {
   const [_, dispatch] = React.useContext(ConfiguratorContext);
   const setTrack = () => dispatch({ type: 'SET_TRACK', payload: id });
 
   return (
     <>
       <text
-        x={8 - width}
+        x={-width * 0.9}
         y={y + 32}
         fill={active ? colors.notes : 'gray'}
         fontFamily="sanf-serif"
