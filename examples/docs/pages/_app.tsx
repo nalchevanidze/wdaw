b23837940178d3e0995020cb4ce859960e197b8f
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-29 11:42:29 +0100

    App

--- examples/docs/pages/_app.tsx
+++ examples/docs/pages/_app.tsx
@@ -1,28 +1,15 @@
 import { AppProps } from 'next/app';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <div
-    style={{
-      width: '100%',
-      background: 'rgb(215 239 253 / 65%)'
-    }}
-  >
+  <div style={{ width: '100%' }}>
     <div
       style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(12, 1fr)',
-        gap: '2em',
         maxWidth: 1260,
         margin: '0 auto',
-        padding: '8rem 2rem',
-        background: 'rgb(243 251 255 / 81%)',
-        border: '2px solid white',
-        borderRadius: 5
+        padding: '3rem'
       }}
     >
-      <div style={{ gridColumn: '3 / 13' }}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </div>
   </div>
 );
