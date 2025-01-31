d9d5980c56e2c344d1f8f2d4a1a9e37c95fe3dd9
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-31 22:29:03 +0100

    add prefix

--- examples/docs/next.config.js
+++ examples/docs/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
+const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
 module.exports = {
   output: 'export',
   reactStrictMode: true,
+  assetPrefix: isDev ? undefined : 'https://nalchevanidze.github.io/wdaw/',
   transpilePackages: ['@wdaw/svg', '@wdaw/ui', '@wdaw/engine', '@wdaw/player']
 };
