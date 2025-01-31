22daab4dc205ae1a9c9806a6213dfe1ce177a74e
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-31 18:07:37 +0100

    icons

--- examples/docs/next.config.js
+++ examples/docs/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   transpilePackages: ['@wdaw/svg', '@wdaw/ui', '@wdaw/engine', '@wdaw/player']
 };
