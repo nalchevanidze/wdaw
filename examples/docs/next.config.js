fec7150082c76abdc69d6441af7ca0125369289d
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-01-31 22:31:01 +0100

    assetPrefix

--- examples/docs/next.config.js
+++ examples/docs/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
-
-const isDev = phase === PHASE_DEVELOPMENT_SERVER
-
 module.exports = {
   output: 'export',
   reactStrictMode: true,
-  assetPrefix: isDev ? undefined : 'https://nalchevanidze.github.io/wdaw/',
+  assetPrefix: 'https://nalchevanidze.github.io/wdaw',
   transpilePackages: ['@wdaw/svg', '@wdaw/ui', '@wdaw/engine', '@wdaw/player']
 };
