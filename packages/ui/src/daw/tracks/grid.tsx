78da2b340604258e25fb6d62d9605ed9b2c045ba
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-01 12:26:58 +0100

    fix grid

--- packages/ui/src/daw/tracks/grid.tsx
+++ packages/ui/src/daw/tracks/grid.tsx
@@ -2,30 +2,41 @@ import * as React from 'react';
 import { NOTE, BLOCK, QUARTER } from '../../common/units';
 import { VerticalLine } from '../../components/vertical-line';
 
-export const Grid: React.FC = () => (
-  <g>
-    <defs>
-      <pattern
-        width={NOTE}
-        height="100%"
-        patternUnits="userSpaceOnUse"
-        id="track-quart"
-      >
-        <VerticalLine x={NOTE} strength={0.1} />
-      </pattern>
-      <pattern
-        width={BLOCK}
-        height="100%"
-        patternUnits="userSpaceOnUse"
-        id="track-key"
-      >
-        <rect width={BLOCK} height="100%" fill="url(#track-quart)" />
-        <VerticalLine x={QUARTER} strength={0.2} />
-        <VerticalLine x={QUARTER * 2} strength={0.4} />
-        <VerticalLine x={QUARTER * 3} strength={0.2} />
-        <VerticalLine x={BLOCK} strength={2} />
-      </pattern>
-    </defs>
-    <rect width="100%" height="100%" fill="url(#track-key)" className="grids" />
-  </g>
-);
+type Props = {
+  strength?: number;
+};
+
+export const Grid: React.FC<Props> = ({ strength = 0.8 }) => {
+  const id = React.useId();
+  const childId = React.useId();
+
+  return (
+    <g>
+      <defs>
+        <pattern
+          width={NOTE}
+          height="100%"
+          patternUnits="userSpaceOnUse"
+          id={childId}
+        >
+          <VerticalLine x={0} strength={0.1} />
+          <VerticalLine x={NOTE} strength={0.1} />
+        </pattern>
+        <pattern
+          width={BLOCK}
+          height="100%"
+          patternUnits="userSpaceOnUse"
+          id={id}
+        >
+          <rect width={BLOCK} height="100%" fill={`url(#${childId})`} />
+          <VerticalLine x={0} strength={strength} />
+          <VerticalLine x={QUARTER} strength={strength / 4} />
+          <VerticalLine x={QUARTER * 2} strength={strength / 2} />
+          <VerticalLine x={QUARTER * 3} strength={strength / 4} />
+          <VerticalLine x={BLOCK} strength={strength} />
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" fill={`url(#${id})`} />
+    </g>
+  );
+};
