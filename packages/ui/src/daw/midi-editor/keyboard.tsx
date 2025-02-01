b7a53213ee9db0aac7b39c53d4ba3b02cc6da967
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-01 21:15:31 +0100

    update

--- packages/ui/src/daw/midi-editor/keyboard.tsx
+++ packages/ui/src/daw/midi-editor/keyboard.tsx
@@ -1,33 +1,36 @@
 import * as React from 'react';
 
-import { BLOCK, NOTE } from '../../common/units';
+import { NOTE } from '../../common/units';
 import { colors } from '../../styles';
 
 type KeysProps = {
-  width: number;
   opacity?: number;
 };
 
-export const Keys: React.FC<KeysProps> = ({ width, opacity = 1 }) => (
+export const Keys: React.FC<KeysProps> = ({ opacity = 1 }) => (
   <g fill="black" fillOpacity={opacity}>
     {[1, 3, 5, 8, 10].map((i) => (
-      <rect key={i} y={i * NOTE} width={width} height={NOTE} />
+      <rect key={i} y={i * NOTE} width="100%" height={NOTE} />
     ))}
   </g>
 );
 
 type Props = { width: number; ocatveHeight: number; count: number };
 
-export const Keyboard: React.FC<Props> = ({ width, ocatveHeight, count = 4 }) => (
+export const Keyboard: React.FC<Props> = ({
+  width,
+  ocatveHeight,
+  count = 4
+}) => (
   <g>
     <defs>
       <pattern
-        width={BLOCK}
+        width="100%"
         height={ocatveHeight}
         patternUnits="userSpaceOnUse"
         id="start_keys"
       >
-        <Keys width={BLOCK} />
+        <Keys />
       </pattern>
     </defs>
     <rect
