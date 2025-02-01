dd4fd19ddc7cbea16c14a0ed9ce7e42548291aef
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-01 11:44:21 +0100

    grid id

--- packages/ui/src/daw/midi-editor/grid.tsx
+++ packages/ui/src/daw/midi-editor/grid.tsx
@@ -5,36 +5,36 @@ import { VerticalLine } from '../../components/vertical-line';
 
 type Props = { ocatveHeight: number; count: number };
 
-export const Grid: React.FC<Props> = ({ ocatveHeight, count }) => (
-  <g>
-    <defs>
-      <pattern
-        width={NOTE}
-        height="100%"
-        patternUnits="userSpaceOnUse"
-        id="quart"
-      >
-        <VerticalLine x={NOTE} strength={0.1} />
-      </pattern>
-      <pattern
-        width={BLOCK}
-        height={ocatveHeight}
-        patternUnits="userSpaceOnUse"
-        id="key"
-      >
-        <Keys width={BLOCK} opacity={0.07} />
-        <rect width={BLOCK} height="100%" fill="url(#quart)" />
-        <VerticalLine x={QUARTER} strength={0.2} />
-        <VerticalLine x={QUARTER * 2} strength={0.4} />
-        <VerticalLine x={QUARTER * 3} strength={0.2} />
-        <VerticalLine x={BLOCK} strength={2} />
-      </pattern>
-    </defs>
-    <rect
-      width="100%"
-      height={ocatveHeight * count}
-      fill="url(#key)"
-      className="grids"
-    />
-  </g>
-);
+export const Grid: React.FC<Props> = ({ ocatveHeight, count }) => {
+  const id = React.useId();
+  const noteId = `note-${id}`;
+
+  return (
+    <g>
+      <defs>
+        <pattern
+          width={NOTE}
+          height="100%"
+          patternUnits="userSpaceOnUse"
+          id={noteId}
+        >
+          <VerticalLine x={NOTE} strength={0.1} />
+        </pattern>
+        <pattern
+          width={BLOCK}
+          height={ocatveHeight}
+          patternUnits="userSpaceOnUse"
+          id="key"
+        >
+          <Keys width={BLOCK} opacity={0.07} />
+          <rect width={BLOCK} height="100%" fill={`url(#${noteId})`} />
+          <VerticalLine x={QUARTER} strength={0.2} />
+          <VerticalLine x={QUARTER * 2} strength={0.4} />
+          <VerticalLine x={QUARTER * 3} strength={0.2} />
+          <VerticalLine x={BLOCK} strength={2} />
+        </pattern>
+      </defs>
+      <rect width="100%" height={ocatveHeight * count} fill={`url(#${id})`} />
+    </g>
+  );
+};
