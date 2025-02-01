5414e5072ca7bd70fa90cfdc3d3af5e1366d0d24
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-01 12:45:03 +0100

    strength

--- packages/ui/src/daw/midi-editor/midi-grid.tsx
+++ packages/ui/src/daw/midi-editor/midi-grid.tsx
@@ -3,12 +3,15 @@ import { NOTE, BLOCK, QUARTER } from '../../common/units';
 import { Keys } from './keyboard';
 import { VerticalLine } from '../../components/vertical-line';
 
-type Props = { ocatveHeight: number; count: number };
+type Props = { ocatveHeight: number; count: number; strength: number };
 
-export const MidiGrid: React.FC<Props> = ({ ocatveHeight, count }) => {
-  const blockId = React.useId();
+export const MidiGrid: React.FC<Props> = ({
+  ocatveHeight,
+  count,
+  strength = 0.8
+}) => {
+  const id = React.useId();
   const noteId = React.useId();
-  const strength = 0.8;
 
   return (
     <g>
@@ -19,13 +22,14 @@ export const MidiGrid: React.FC<Props> = ({ ocatveHeight, count }) => {
           patternUnits="userSpaceOnUse"
           id={noteId}
         >
+          <VerticalLine x={0} strength={0.1} />
           <VerticalLine x={NOTE} strength={0.1} />
         </pattern>
         <pattern
           width={BLOCK}
           height={ocatveHeight}
           patternUnits="userSpaceOnUse"
-          id={blockId}
+          id={id}
         >
           <Keys width={BLOCK} opacity={0.07} />
           <rect width={BLOCK} height="100%" fill={`url(#${noteId})`} />
@@ -36,11 +40,7 @@ export const MidiGrid: React.FC<Props> = ({ ocatveHeight, count }) => {
           <VerticalLine x={BLOCK} strength={strength} />
         </pattern>
       </defs>
-      <rect
-        width="100%"
-        height={ocatveHeight * count}
-        fill={`url(#${blockId})`}
-      />
+      <rect width="100%" height={ocatveHeight * count} fill={`url(#${id})`} />
     </g>
   );
 };
