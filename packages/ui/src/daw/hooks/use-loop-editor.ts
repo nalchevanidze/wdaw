d8d47045cb5792af38977a10255d813964f65462
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-01 21:17:44 +0100

    update

--- packages/ui/src/daw/hooks/use-loop-editor.ts
+++ packages/ui/src/daw/hooks/use-loop-editor.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Area, Maybe } from '../types';
 import { distanceX } from '../utils/area';
-import { NOTE } from '../../common/units';
+import { STEP } from '../../common/units';
 import { useTrack } from './use-track';
 
 export type LoopTarget = 'start' | 'end';
@@ -34,7 +34,7 @@ export const useLoop = () => {
   };
 
   const move = (area: Area) => {
-    const time = distanceX(area, NOTE) * 2;
+    const time = distanceX(area, STEP);
 
     if (origin === undefined) return;
 
