ba35c63c3bc58c5bd8af138929260205532da7e9
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-02 11:44:01 +0100

    0.8

--- packages/ui/src/daw/synth/envelopes/envelope-graphic.tsx
+++ packages/ui/src/daw/synth/envelopes/envelope-graphic.tsx
@@ -121,10 +121,15 @@ const EnvelopeConsumer: React.FC<Props> = ({ id }) => {
 
 const padding = 5;
 const width = 180;
-const height= 100;
+const height = 100;
 
 const EnvelopeGraphic = (props: Props) => (
-  <Svg viewBox={[-padding, -padding, width + padding * 2, height + padding * 2]} width={width} height={height}>
+  <Svg
+    viewBox={[-padding, -padding, width + padding * 2, height + padding * 2]}
+    width={width}
+    height={height}
+    zoom={0.8}
+  >
     <EnvelopeConsumer {...props} />
   </Svg>
 );
