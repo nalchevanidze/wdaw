fb3825b8ab71d539618ee0926205cb05df9491e7
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-03 09:32:58 +0100

    update

--- packages/ui/src/daw/utils/math.ts
+++ packages/ui/src/daw/utils/math.ts
@@ -1,8 +1,8 @@
+import {  Range } from '@wdaw/svg';
+
 export const unitInterval = (n: number) => Math.min(Math.max(n, 0), 1);
 
 export const positive = (n: number) => Math.max(0, n);
-
-export type Range = [number, number];
 
 export const intRange = (value: number, [min, max]: Range) => {
   const size = max - min;
