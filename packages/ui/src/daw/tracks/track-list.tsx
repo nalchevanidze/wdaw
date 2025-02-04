0e4f8a9196c787015babdcc978ab0d481484f9e9
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-04 16:24:43 +0100

    track list

--- packages/ui/src/daw/tracks/track-list.tsx
+++ packages/ui/src/daw/tracks/track-list.tsx
@@ -45,57 +45,53 @@ export const Tracks: React.FC<{
   });
 
   return (
-    <>
-      <NoteGrid size={rulerSize} />
-      <Timeline height={timelineHeight} size={rulerSize} />
-      <g
-        onMouseMove={dragging.onMove}
-        onMouseLeave={dragging.end}
-        onMouseUp={dragging.end}
-      >
-        <rect
-          y={0}
-          onMouseDown={dragging.onBackground}
-          height={canvasHeight}
-          width="100%"
-          opacity={0}
-        />
-        {tracks.tracks.map(({ midi, name }, i) => {
-          const y = i * trackHeight;
-          const state = all.find( s=>  s.id === i) ?? midi
+    <g
+      onMouseMove={dragging.onMove}
+      onMouseLeave={dragging.end}
+      onMouseUp={dragging.end}
+    >
+      <rect
+        y={0}
+        onMouseDown={dragging.onBackground}
+        height={canvasHeight}
+        width="100%"
+        opacity={0}
+      />
+      {tracks.tracks.map(({ midi, name }, i) => {
+        const y = i * trackHeight;
+        const state = all.find((s) => s.id === i) ?? midi;
 
-          return (
-            <g key={i}>
-              <MidiLoop
-                y={y}
-                start={state?.start }
-                end={state?.end}
-                midi={midi}
-                height={trackHeight}
-                startMove={(e) => dragging.onStart('move')(e, i)}
-                startScale={(e) => dragging.onStart('scale')(e, i)}
-              />
-              <Panel
-                active={i === tracks.currentTrack}
-                name={name}
-                id={i}
-                width={panelWidth}
-                y={y}
-                height={trackHeight}
-              />
-            </g>
-          );
-        })}
-        <rect
-          y={-timelineHeight}
-          height={canvasHeight + timelineHeight}
-          width={1}
-          x={player.time}
-          fill={colors.critical}
-        />
-        {dragging.area ? <SelectionArea area={dragging.area} /> : null}
-      </g>
-    </>
+        return (
+          <g key={i}>
+            <MidiLoop
+              y={y}
+              start={state?.start}
+              end={state?.end}
+              midi={midi}
+              height={trackHeight}
+              startMove={(e) => dragging.onStart('move')(e, i)}
+              startScale={(e) => dragging.onStart('scale')(e, i)}
+            />
+            <Panel
+              active={i === tracks.currentTrack}
+              name={name}
+              id={i}
+              width={panelWidth}
+              y={y}
+              height={trackHeight}
+            />
+          </g>
+        );
+      })}
+      <rect
+        y={-timelineHeight}
+        height={canvasHeight + timelineHeight}
+        width={1}
+        x={player.time}
+        fill={colors.critical}
+      />
+      {dragging.area ? <SelectionArea area={dragging.area} /> : null}
+    </g>
   );
 };
 
@@ -112,6 +108,8 @@ export const TrackList = () => {
         paddingLeft={panelWidth}
         paddingTop={timelineHeight}
       >
+        <NoteGrid size={rulerSize} />
+        <Timeline height={timelineHeight} size={rulerSize} />
         <Tracks canvasHeight={canvasHeight} />
       </Svg>
     </div>
