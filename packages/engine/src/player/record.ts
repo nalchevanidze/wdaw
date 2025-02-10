70c2f41ee699207cfd35204cdf335a4a020c668c
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-10 19:52:37 +0100

    public

--- packages/engine/src/player/record.ts
+++ packages/engine/src/player/record.ts
@@ -7,19 +7,22 @@ export class RecordLoop {
     this.buffer = Array(size).fill(undefined);
   }
 
-  get = (index: number) => this.buffer[index];
+  
+  
+  public get = (index: number) => this.buffer[index];
 
-  private lookup = (i: number) => (this.buffer[i] = this.buffer[i] ?? {});
-
-  start = (i: number, key: number) => {
+  public start = (i: number, key: number) => {
     const step = this.lookup(i);
     step.start = step.start ?? [];
     step.start.push(key);
   };
 
-  end = (i: number, key: number) => {
+  public end = (i: number, key: number) => {
     const step = this.lookup(i);
     step.end = step.end ?? [];
     step.end.push(key);
   };
+
+  private lookup = (i: number) => (this.buffer[i] = this.buffer[i] ?? {});
+
 }
