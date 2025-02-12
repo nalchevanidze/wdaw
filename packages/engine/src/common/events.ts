caca841d48a0d70ad6a057a3efebbfdd68f40846
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-12 14:52:12 +0100

    events

--- packages/engine/src/common/events.ts
+++ packages/engine/src/common/events.ts
@@ -3,6 +3,13 @@ type EventTypes = {
   timeChanged: number;
 };
 
+type Parsers = { [K in EventName]: (i: any) => EventTypes[K] | undefined };
+
+const parsers: Parsers = {
+  isPlayingChanged: (v) => (typeof v !== 'boolean' ? v : undefined),
+  timeChanged: (v) => (typeof v !== 'number' ? v : undefined)
+};
+
 export type EventName = keyof EventTypes;
 
 export type EventHandler<T extends EventName> = (e: EventTypes[T]) => void;
@@ -10,20 +17,11 @@ export type EventHandler<T extends EventName> = (e: EventTypes[T]) => void;
 export const mkEvent = <T extends EventName>(name: T, value: EventTypes[T]) =>
   new CustomEvent(name, { detail: value });
 
-type Parsers = { [K in EventName]: (i: any) => EventTypes[K] | undefined };
-
-const events: Parsers = {
-  isPlayingChanged: (v) => (typeof v !== 'boolean' ? v : undefined),
-  timeChanged: (v) => (typeof v !== 'number' ? v : undefined)
-};
-
 export const makeHandler =
   <N extends EventName>(name: N, f: EventHandler<N>) =>
-  (e: Event): void => {
-    if (!(e instanceof CustomEvent)) return undefined;
-
-    const value = events[name](e.detail);
-
+  (event: Event): void => {
+    if (!(event instanceof CustomEvent)) return undefined;
+    const value = parsers[name](event.detail);
     if (!value) return;
 
     f(value);
