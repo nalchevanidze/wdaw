0d4aa0355cd43a265efcbf1e6abd38eed7c87cbf
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-14 11:37:41 +0100

    use mouse event

---
+++ packages/ui/src/daw/hooks/use-mouse-event.ts
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+type Handler = (m: 'MOVE' | 'UP') => (e: MouseEvent) => void;
+
+export const usMouseEvent = (f: Handler, args: unknown[] = []) => {
+  const onMove = f('MOVE');
+  const onUp = f('UP');
+
+  React.useEffect(() => {
+    document.addEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', onUp);
+
+    return () => {
+      document.removeEventListener('mousemove', onMove);
+      document.removeEventListener('mouseup', onUp);
+    };
+  }, args);
+};
