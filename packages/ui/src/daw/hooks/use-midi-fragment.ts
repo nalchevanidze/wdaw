c69b9deb3cf3c4597fee672a4fa08387656d3628
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-16 12:43:37 +0100

    id

--- packages/ui/src/daw/hooks/use-midi-fragment.ts
+++ packages/ui/src/daw/hooks/use-midi-fragment.ts
@@ -3,13 +3,15 @@ import { DawDispatch } from '../types';
 import { DawApiContext } from '../../context/state';
 import { MidiFragment } from '@wdaw/engine';
 
-export const useMidiFragment = (): [MidiFragment & { id: string }, DawDispatch] => {
+export const useMidiFragment = (): [
+  MidiFragment & { id: string },
+  DawDispatch
+] => {
   const [{ currentFragment, midiFragments }, dispatch] =
     React.useContext(DawApiContext);
 
   const id = currentFragment;
+  const { notes, loop } = midiFragments[id];
 
-  const fragment = midiFragments[id];
-
-  return [{ ...fragment, id }, dispatch];
+  return [{ notes, loop, id }, dispatch];
 };
