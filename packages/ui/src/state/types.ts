99d7e368305f517da6db0956366b44b42069a967
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-02-27 12:58:24 +0100

    EngineAction

--- packages/ui/src/state/types.ts
+++ packages/ui/src/state/types.ts
@@ -25,44 +25,37 @@ type Player<T extends string, P = {}> = ADT<'PLAYER', T, P>;
 type Store<T extends string, P = {}> = ADT<'STORE', T, P>;
 type Keyboard<T extends string, P = {}> = ADT<'KEYBOARD', T, P>;
 
-type PRESET =
+export type EngineAction =
   | Preset<'SET_WAVE', { id: WAVE_ID; payload: number }>
   | Preset<'SET_ENVELOPE', { id: ENVELOPE_ID; payload: Partial<Envelope> }>
   | Preset<'SET_FILTER', { id: FILTER_ID; payload: number }>
   | Preset<'SET_SEQUENCE', { payload: Sequence }>
   | Preset<'TOGGLE_MODULE', { id: 'filter' | 'sequence' }>
   | Preset<'NEW_PRESET', { trackId: number }>
-  | Preset<'ASSIGN_TO_TRACK', { trackId: number; presetId: string }>;
-
-type TRACK =
+  | Preset<'ASSIGN_TO_TRACK', { trackId: number; presetId: string }>
+  // TRACK
   | Track<'SET_TRACK', { id: number; payload: Partial<TrackInput> }>
   | Track<'SET_CURRENT', { payload: number }>
-  | Track<'NEW_TRACK'>;
-
-type MIDI =
+  | Track<'NEW_TRACK'>
+  // MIDI
   | Midi<'NEW_FRAGMENT'>
   | Midi<'SET_CURRENT_FRAGMENT', { payload: string }>
   | Midi<'SET_MIDI_REFS', { payload: MidiRef[] }>
   | Midi<'SET_FRAGMENT', { id: string; payload: Partial<MidiFragment> }>
-  | Midi<'SET_MIDI_REF', { id: string; payload: Partial<MidiRef> }>;
-
-type PLAYER =
+  | Midi<'SET_MIDI_REF', { id: string; payload: Partial<MidiRef> }>
+  // PLAYER
   | Player<'SET_TIME', { payload: number }>
   | Player<'SET_BPM', { payload: number }>
   | Player<'PLAY'>
   | Player<'STOP'>
-  | Player<'PAUSE'>;
-
-type KEYBOARD =
+  | Player<'PAUSE'>
+  // KEYBOARD
   | Keyboard<'KEY_UP', { payload: number }>
-  | Keyboard<'KEY_DOWN', { payload: number }>;
-
-type STORE =
+  | Keyboard<'KEY_DOWN', { payload: number }>
+  // STORE
   | Store<'SAVE'>
   | Store<'RESET'>
   | Store<'LOAD', { payload: DAWState }>;
-
-export type EngineAction = KEYBOARD | PRESET | TRACK | MIDI | PLAYER | STORE;
 
 type UIState = {
   currentFragment: string;
