65da20cc5dc7718fa2277d3348a47456c7848a25
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-01 13:55:18 +0100

    types

--- packages/ui/src/state/utils.ts
+++ packages/ui/src/state/utils.ts
@@ -20,43 +20,39 @@ export const loadState = () => {
   return v ? (JSON.parse(v) as State) : undefined;
 };
 
-export const mapLib = <T>(
-  lib: Record<string, T>,
-  id: number,
-  f: (a: T) => Partial<T>
-): Record<string, T> => ({ ...lib, [id]: { ...lib[id], ...f(lib[id]) } });
+type Lib<T> = Record<string, T>;
+
+type Fun<T> = (a: T) => Partial<T>;
+
+const mapLib = <T>(lib: Lib<T>, id: string, f: Fun<T>): Lib<T> => ({
+  ...lib,
+  [id]: { ...lib[id], ...f(lib[id]) }
+});
 
 export const mapMidiFragment = (
   midiFragments: EngineState['midiFragments'],
   id: string,
-  f: (a: MidiFragment) => Partial<MidiFragment>
-) => ({
-  midiFragments: {
-    ...midiFragments,
-    [id]: { ...midiFragments[id], ...f(midiFragments[id]) }
-  }
-});
-
-export const mapTrack = (
-  tracks: EngineState['tracks'],
-  id: number,
-  f: (a: TrackInput) => Partial<TrackInput>
-): Partial<EngineState> => ({
-  tracks: tracks.map((t, i) => (id === i ? { ...t, ...f(t) } : t))
-});
+  f: Fun<MidiFragment>
+) => ({ midiFragments: mapLib(midiFragments, id, f) });
 
 export const mapPreset = (
   presets: EngineState['presets'],
   id: string,
-  f: (a: Preset) => Partial<Preset>
-) => ({
-  presets: { ...presets, [id]: { ...presets[id], ...f(presets[id]) } }
+  f: Fun<Preset>
+) => ({ presets: mapLib(presets, id, f) });
+
+export const mapTrack = (
+  tracks: EngineState['tracks'],
+  id: number,
+  f: Fun<TrackInput>
+): Partial<EngineState> => ({
+  tracks: tracks.map((t, i) => (id === i ? { ...t, ...f(t) } : t))
 });
 
 export const mapMidiRefs = (
   midiRefs: EngineState['midiRefs'],
   id: string,
-  f: (a: MidiRef) => Partial<MidiRef>
+  f: Fun<MidiRef>
 ) => ({
   midiRefs: midiRefs.map((m) => (m.id === id ? { ...m, ...f(m) } : m))
 });
