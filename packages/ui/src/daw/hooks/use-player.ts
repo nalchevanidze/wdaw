8e525391060880350f247bb520281c792bf152cd
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-07 11:30:25 +0100

    setBPM

--- packages/ui/src/daw/hooks/use-player.ts
+++ packages/ui/src/daw/hooks/use-player.ts
@@ -4,17 +4,16 @@ import { DawApiContext } from '../../context/state';
 export const usePlayer = () => {
   const { isPlaying, bpm, time, engine, dispatch } = useContext(DawApiContext);
 
-  const setBPM = (value: number) =>
-    dispatch({ type: 'PLAYER/SET_BPM', payload: value });
-
   const toggle = () => (isPlaying ? engine.pause() : engine.play());
-
   const stop = engine.stop;
   const setTime = engine.setTime;
 
   const save = () => dispatch({ type: 'STORE/SAVE' });
 
   const reset = () => dispatch({ type: 'STORE/RESET' });
+
+  const setBPM = (value: number) =>
+    dispatch({ type: 'PLAYER/SET_BPM', payload: value });
 
   return { isPlaying, bpm, time, setBPM, setTime, toggle, stop, save, reset };
 };
