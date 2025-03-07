fe82e6d09b89bc94a84b63b502a0aad4a1391a92
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-07 10:38:40 +0100

    engine ref

--- packages/ui/src/hooks/use-engine.ts
+++ packages/ui/src/hooks/use-engine.ts
@@ -1,38 +1,33 @@
 import { useEffect, useReducer, useRef, useState } from 'react';
 import { SynthEngine } from '@wdaw/engine';
-import { State, EngineAction, KeyboardAPI } from '../state/types';
 import { dawState } from '../state/defs';
 import { loadState } from '../state/utils';
 import { reducer } from '../state';
 import { engineEffects } from '../state/engine-effects';
 
 export const useEngine = () => {
-  const ref = useRef<(s: State, a: EngineAction) => State>((a) => a);
-
-  const keyboard = useRef<KeyboardAPI>({ startNote() {}, endNote() {} });
+  const ref = useRef<SynthEngine>(new SynthEngine());
 
   const [time, setTime] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     const engine = new SynthEngine();
+    ref.current = engine;
+
     engine.addEventListener('isPlayingChanged', setIsPlaying);
     engine.addEventListener('timeChanged', setTime);
-
-    keyboard.current = engine;
-
-    ref.current = (state, action) => {
-      const newState = reducer(state, action);
-      engineEffects(newState, engine, action);
-      return newState;
-    };
 
     dispatch({ type: 'STORE/LOAD', payload: loadState() ?? state });
 
     return engine.destroy;
   }, []);
 
-  const [state, dispatch] = useReducer(ref.current, dawState());
+  const [state, dispatch] = useReducer((state, action) => {
+    const newState = reducer(state, action);
+    engineEffects(newState, ref.current, action);
+    return newState;
+  }, dawState());
 
-  return { ...state, time, isPlaying, dispatch, keyboard: keyboard.current };
+  return { ...state, time, isPlaying, dispatch, keyboard: ref.current };
 };
