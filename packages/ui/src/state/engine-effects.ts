5e377ea308f44e0148969b2a492933f73c8d8736
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-07 10:05:47 +0100

    SynthEngine

---
+++ packages/ui/src/state/engine-effects.ts
@@ -0,0 +1,48 @@
+import { EngineState, SynthEngine } from '@wdaw/engine';
+import { EngineAction } from './types';
+
+export const engineEffects = (
+  { tracks, midiFragments, presets, midiRefs }: EngineState,
+  engine: SynthEngine,
+  action: EngineAction
+): void => {
+  switch (action.type) {
+    // Player
+    case 'PLAYER/PLAY':
+      return engine.play();
+    case 'PLAYER/PAUSE':
+      return engine.pause();
+    case 'PLAYER/STOP':
+      return engine.stop();
+    case 'PLAYER/SET_TIME':
+      return engine.setTime(action.payload);
+    case 'PLAYER/SET_BPM':
+      return engine.setBPM(action.payload);
+    // Midi
+    case 'MIDI/SET_MIDI_REF':
+    case 'MIDI/SET_FRAGMENT':
+    case 'MIDI/SET_MIDI_REFS':
+      return engine.setMidis(midiRefs, midiFragments);
+    // Track
+    case 'TRACK/SET_TRACK':
+      return action.payload.gain
+        ? engine.setGain(action.id, action.payload.gain)
+        : undefined;
+    case 'TRACK/NEW_TRACK':
+      engine.setTracks({ tracks, midiFragments, presets, midiRefs });
+      return;
+    // Preset
+    case 'PRESET/SET_SEQUENCE':
+    case 'PRESET/TOGGLE_MODULE':
+    case 'PRESET/SET_ENVELOPE':
+    case 'PRESET/SET_WAVE':
+    case 'PRESET/SET_FILTER':
+    case 'PRESET/ASSIGN_TO_TRACK':
+      return engine.setPreset(action.trackId, presets[action.presetId]);
+    // Store
+    case 'STORE/LOAD':
+      engine.setTracks(action.payload);
+      engine.setBPM(action.payload.bpm);
+      return;
+  }
+};
