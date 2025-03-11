f12c783fe2fd73c741251757ed66704233c75b62
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-11 22:45:02 +0100

    DynamicValue

--- packages/ui/src/daw/tracks/bpmEditor.tsx
+++ packages/ui/src/daw/tracks/bpmEditor.tsx
@@ -1,52 +1,24 @@
 import * as React from 'react';
 import { Point } from '@wdaw/svg';
-import { LineEditor } from '../../components/line-editor';
 import { usePlayer } from '../hooks/use-player';
+import { DynamicValue } from './dynamic-value';
 
 type Props = {
   height: number;
   width: number;
 };
 
-const max = 200;
-const min = 40;
-const scaleY = max - min;
-
 export const BpmEditor: React.FC<Props> = ({ height, width }) => {
   const { bpm, setBPM } = usePlayer();
 
-  const points = (bpm.type === 'dynamic' ? bpm.value : []).map((p, i) => ({
-    x: p.index / width,
-    y: (p.value - min) / scaleY,
-    id: i.toString()
-  }));
-
-  const update = (ps: Point[]) =>
-    setBPM({
-      type: 'dynamic',
-      value: ps
-        .sort((a, b) => a.x - b.x)
-        .map(({ x, y }) => ({ index: x * width, value: min + y * scaleY }))
-    });
-
-  const setPoint = (id: string, point: Point) =>
-    update(points.map((p) => (p.id === id ? { ...p, ...point } : p)));
-
-  const insert = (p: Point) => update([...points, p]);
-
   return (
-    <LineEditor
-      insert={insert}
+    <DynamicValue
+      max={200}
+      min={40}
       height={height}
       width={width}
-      onMove={setPoint}
-      controlers={[
-        { x: 0, y: 0 },
-        { x: 0, y: points[0].y },
-        ...points,
-        { x: 1, y: points[points.length - 1].y },
-        { x: 1, y: 0 }
-      ]}
+      setValues={(value) => setBPM({ type: 'dynamic', value })}
+      values={bpm.type === 'dynamic' ? bpm.value : []}
     />
   );
 };
---
+++ packages/ui/src/daw/tracks/dynamic-value.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Point } from '@wdaw/svg';
+import { LineEditor } from '../../components/line-editor';
+
+type Value = {
+  index: number;
+  value: number;
+};
+
+type Props = {
+  height: number;
+  width: number;
+  setValues(vs: Value[]): void;
+  values: Value[];
+  min: number,
+  max: number
+};
+
+
+
+export const DynamicValue: React.FC<Props> = ({
+  height,
+  width,
+  setValues,
+  values,
+  max = 1,
+  min = 0
+}) => {
+  const scaleY = max - min;
+  const points = values.map((p, i) => ({
+    x: p.index / width,
+    y: (p.value - min) / scaleY,
+    id: i.toString()
+  }));
+
+  const update = (ps: Point[]) =>
+    setValues(
+      ps
+        .sort((a, b) => a.x - b.x)
+        .map(({ x, y }) => ({ index: x * width, value: min + y * scaleY }))
+    );
+
+  const setPoint = (id: string, point: Point) =>
+    update(points.map((p) => (p.id === id ? { ...p, ...point } : p)));
+
+  const insert = (p: Point) => update([...points, p]);
+
+  return (
+    <LineEditor
+      insert={insert}
+      height={height}
+      width={width}
+      onMove={setPoint}
+      controlers={[
+        { x: 0, y: 0 },
+        { x: 0, y: points[0].y },
+        ...points,
+        { x: 1, y: points[points.length - 1].y },
+        { x: 1, y: 0 }
+      ]}
+    />
+  );
+};
