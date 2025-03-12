be31fa4c8df1307f8654fe2ce9e44dab6aca4c90
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-12 10:23:20 +0100

    update

--- packages/engine/src/player/utils/dynamic-value.ts
+++ packages/engine/src/player/utils/dynamic-value.ts
@@ -1,30 +1,27 @@
 import { ControlPoint } from '../../state/state';
 
-const fill = (size: number, list: ControlPoint[]): number[] => {
+const fill = (size: number, [head, ...list]: ControlPoint[]): number[] => {
   const points = Array(size).fill(0);
 
-  list.reduce(
-    (a, b) => {
-      const diff = b.index - a.index;
+  list.reduce((a, b) => {
+    const diff = b.index - a.index;
 
-      if (diff === 0) {
-        points[b.index] = b.value;
-        return b;
-      }
-
-      const step = (b.value - a.value) / diff;
-
-      let value = a.value;
-
-      for (let i = a.index; i < b.index; i++) {
-        value += step;
-        points[i] = value;
-      }
-
+    if (diff === 0) {
+      points[b.index] = b.value;
       return b;
-    },
-    { index: 0, value: 100 }
-  );
+    }
+
+    const step = (b.value - a.value) / diff;
+
+    let value = a.value;
+
+    for (let i = a.index; i < b.index; i++) {
+      value += step;
+      points[i] = value;
+    }
+
+    return b;
+  }, head);
 
   return points;
 };
@@ -39,10 +36,10 @@ export class DynamicValue {
 
     this.start = sorted[0];
     this.end = sorted[sorted.length - 1];
-    
+
     this.list = fill(this.end.index, sorted);
 
-    console.log(this.list)
+    console.log(this.list);
   }
 
   get = (time: number) => {
