cc8938ccab0a3ff8744009aeb5193b9a0ce6cbdd
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-03-13 14:40:59 +0100

    clear

--- packages/engine/src/common/events.ts
+++ packages/engine/src/common/events.ts
@@ -25,16 +25,35 @@ const parsers: Parsers = {
 
 type Handler<N extends EName> = (e: EType<N>) => void;
 
+const makeParser =
+  <N extends EName>(
+    name: N,
+    f: Handler<N>
+  ): EventListenerOrEventListenerObject =>
+  (event) => {
+    if (!(event instanceof CustomEvent)) return undefined;
+    const value = parsers[name](event.detail);
+    return value === undefined ? undefined : f(value);
+  };
+
 export class EngineEvents {
   private target = new EventTarget();
+  private events = new Set<[string, EventListenerOrEventListenerObject]>();
 
   public dispatch = <T extends EName>(name: T, value: Events[T]) =>
     this.target.dispatchEvent(new CustomEvent(name, { detail: value }));
 
-  public addEventListener = <N extends EName>(name: N, f: Handler<N>) =>
-    this.target.addEventListener(name, (event) => {
-      if (!(event instanceof CustomEvent)) return undefined;
-      const value = parsers[name](event.detail);
-      return value === undefined ? undefined : f(value);
+  public addEventListener = <N extends EName>(name: N, f: Handler<N>) => {
+    const func = makeParser(name, f);
+    this.events.add([name, func]);
+    this.target.addEventListener(name, func);
+  };
+
+  public clear() {
+    this.events.forEach(([name, f]) => {
+      this.target.addEventListener(name, f);
     });
+
+    this.events.clear();
+  }
 }
